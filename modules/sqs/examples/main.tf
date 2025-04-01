eef2e8e9edf4168c156049dc6e48568b0c2062de
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-04-01 09:17:56 +0100

    fix: move namespace_module_release into a local variable

--- modules/sqs/examples/main.tf
+++ modules/sqs/examples/main.tf
@@ -1,9 +1,5 @@
-locals {
-  module_source = "github.com/ministryofjustice/cloud-platform-terraform-test-applications/modules/sqs?ref=main"
-}
-
 module "sqs_application" {
-  source = local.module_source
+  source = "github.com/ministryofjustice/cloud-platform-terraform-test-applications/modules/sqs?ref=main"
 
   namespace = "module-sqstest-namespace"
   module_release = "v5.1.0"
