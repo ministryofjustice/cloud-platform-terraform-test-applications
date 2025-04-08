b54aca91ee9b7112f5e7cece9ee7b32b6000468b
folarin oyenuga <folarin.justice@digital.justice.gov.uk>
2025-04-08 16:52:27 +0100

    fix: update SQS module configuration to match v5.1.0 interface
    - Remove unsupported arguments (module_release, namespace_module_release, etc.)
    - Add required arguments (sqs_name, environment_name)
    - Fix variable types (is_production as string)
    - Add encrypt_sqs_kms configuration

--- modules/sqs/examples/main.tf
+++ modules/sqs/examples/main.tf
@@ -1,18 +1,25 @@
 module "sqs_application" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-sqs?ref=5.1.0"
 
-  namespace = "module-sqstest-namespace"
-  module_release = "5.1.0"
-  namespace_module_release = "v5.1.0"
-  route53_enabled = "false"
-  namespace_enabled = true
-  rolebinding_enabled = true
-  
-  # Required SQS module variables
+  # Required arguments
+  sqs_name = "test-queue"
+  environment_name = "test"
+
+  # Tags (all required)
   business_unit = "test"
   application = "test-app"
-  is_production = false
+  is_production = "false"  # Changed to string as per module requirements
   team_name = "test-team"
-  environment = "test"
+  namespace = "module-sqstest-namespace"
   infrastructure_support = "test@example.com"
+
+  # Optional SQS configuration
+  encrypt_sqs_kms = true
+  # visibility_timeout_seconds = 30  # Uncomment if you need to override defaults
+  # message_retention_seconds = 345600
+  # max_message_size = 262144
+  # delay_seconds = 0
+  # receive_wait_time_seconds = 0
+  # fifo_queue = false
+  # kms_data_key_reuse_period_seconds = 300
 }
